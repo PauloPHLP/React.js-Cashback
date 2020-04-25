ffd866ad8c423c3d2ec87776736ed78647cf8db9
Paulo Lima <paulo.phlp@outlook.com>
2020-04-25 01:44:58 -0300

    Adding transitions between routes.

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
+import { AnimatedSwitch } from 'react-router-transition';
 import Logon from '../pages/Logon/Logon';
 import RegisterUser from '../pages/RegisterUser/RegisterUser';
 import Sales from '../pages/Sales/Sales';
@@ -9,16 +10,23 @@ function Routes() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path="/" component={Logon} exact />
-        <Route path="/registrar" component={RegisterUser} />
-        <Route path="/vendas" component={Sales} />
-        <Route path="/gerenciar" component={SaleManagement} />
-        <Route
-          path="*"
-          component={(props) => (
-            <Redirect to={{ pathname: '/', search: props.location.search }} />
-          )}
-        />
+        <AnimatedSwitch
+          atEnter={{ opacity: 0 }}
+          atLeave={{ opacity: 0 }}
+          atActive={{ opacity: 1 }}
+          className="switch-wrapper"
+        >
+          <Route path="/" component={Logon} exact />
+          <Route path="/registrar" component={RegisterUser} />
+          <Route path="/vendas" component={Sales} />
+          <Route path="/gerenciar" component={SaleManagement} />
+          <Route
+            path="*"
+            component={(props) => (
+              <Redirect to={{ pathname: '/', search: props.location.search }} />
+            )}
+          />
+        </AnimatedSwitch>
       </Switch>
     </BrowserRouter>
   );
