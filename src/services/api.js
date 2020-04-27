0b52d76fd60abceb82c1abfc5d2d86e2b2770ab4
Paulo Lima <paulo.phlp@outlook.com>
2020-04-27 15:09:35 -0300

    Adding CORS fix.

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,20 @@
+/* eslint-disable no-param-reassign */
 import axios from 'axios';
 
+const CASHBACK_API_URL =
+  'https://mdaqk8ek5j.execute-api.us-east-1.amazonaws.com/v1';
 const api = axios.create({
-  // Using https://cors-anywhere.herokuapp.com/ to prevent CORS errors.
-  baseURL:
-    'https://cors-anywhere.herokuapp.com/https://mdaqk8ek5j.execute-api.us-east-1.amazonaws.com/v1'
+  baseURL: CASHBACK_API_URL
 });
+
+api.interceptors.request.use(
+  (config) => {
+    config.headers.Pragma = 'no-cache';
+    config.headers['Access-Control-Allow-Origin'] = '*';
+
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
 
 export default api;
