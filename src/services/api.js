0a62a0a11df907c85a641283fcd377771e7a2c32
Paulo Lima <paulo.phlp@outlook.com>
2020-04-28 15:23:37 -0300

    LogonHandler fix.

--- src/services/api.js
+++ src/services/api.js
@@ -1,10 +1,19 @@
 /* eslint-disable no-param-reassign */
 import axios from 'axios';
 
-const API_URL =
-  'https://cors-anywhere.herokuapp.com/https://mdaqk8ek5j.execute-api.us-east-1.amazonaws.com/v1';
+const API_URL = 'https://mdaqk8ek5j.execute-api.us-east-1.amazonaws.com/v1';
 const api = axios.create({
   baseURL: API_URL
 });
+
+api.interceptors.request.use(
+  (config) => {
+    config.headers.Pragma = 'no-cache';
+    config.headers['Access-Control-Allow-Origin'] = '*';
+
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
 
 export default api;
